a15e8109fec2715f183febb52c4579a7357c55b6
luanyiping <75729730+luanyiping@users.noreply.github.com>
2021-03-05 16:02:47 +0800

    Update 函数重载.cpp

--- 函数重载.cpp
+++ 函数重载.cpp
@@ -1,28 +1,46 @@
 #include<iostream>
 using namespace std;
-// ��������,������ͬһ��������,�����Ĳ���������ͬ ���� ���Ͳ�ͬ ���� ˳��ͬ
-// ����ֵ������Ϊ�������ص�����
+// 函数重载,必须在同一个作用域,函数的参数个数不同 或者 类型不同 或者 顺序不同
+// 返回值不能作为函数重载的条件
 void func()
 {
-	cout << "�޲�����func()" << endl;
+	cout << "无参数的func()" << endl;
 }
 void func(int a)
 {
-	cout << "�в�����func(int a)" << endl;
+	cout << "有参数的func(int a)" << endl;
 }
 void func(double a)
 {
-	cout << "�в�����func(double a)" << endl;
+	cout << "有参数的func(double a)" << endl;
 }
 void func(double a, int b)
 {
-	cout << "�в�����func(double a,int b)" << endl;
+	cout << "有参数的func(double a,int b)" << endl;
 }
 void func(int a, double b)
 {
-	cout << "�в�����func(int a,double b)" << endl;
+	cout << "有参数的func(int a,double b)" << endl;
+}
+// 当函数重载碰上默认参数时，注意二义性的问题
+void func(int a, int b = 10)
+{
+
+}
+void func(int a)
+{
+
 }
 
+// 引用重载
+void func(int& a)
+{
+
+}
+void func(const int& a)
+{
+
+}
 void test01()
 {
 	func();
